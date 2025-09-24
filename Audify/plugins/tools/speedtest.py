6f423ee684e8e007296885bb0fa49a083db7b22d
STRXI01 <rizalpro598@gmail.com>
2025-09-24 18:12:50 +0530

    Update speedtest.py

--- Audify/plugins/tools/speedtest.py
+++ Audify/plugins/tools/speedtest.py
@@ -25,21 +25,59 @@ def testspeed(m, _):
     return result
 
 
-@app.on_message(filters.command(["speedtest", "spt"], prefixes=["/", "!", "%", ",", "", ".", "@", "#"]) & SUDOERS)
-@language 
-async def speedtest_function(client, message: Message, _):
-    m = await message.reply_text(_["server_11"])
-    loop = asyncio.get_event_loop()
-    result = await loop.run_in_executor(None, testspeed, m, _)
-    output = _["server_15"].format(
-        result["client"]["isp"],
-        result["client"]["country"],
-        result["server"]["name"],
-        result["server"]["country"],
-        result["server"]["cc"],
-        result["server"]["sponsor"],
-        result["server"]["latency"],
-        result["ping"],
-    )
-    msg = await message.reply_photo(photo=result["share"], caption=output)
-    await m.delete()
+async def run_speedtest(m: Message):
+    try:
+        st = Speedtest()
+        await m.edit_text("<blockquote><b>ꜰɪɴᴅɪɴɢ ʙᴇꜱᴛ ꜱᴇʀᴠᴇʀ...</b></blockquote>")
+        st.get_best_server()
+        
+        await m.edit_text("<blockquote><b>ᴛᴇꜱᴛɪɴɢ ᴅᴏᴡɴʟᴏᴀᴅ ꜱᴘᴇᴇᴅ...</b></blockquote>")
+        st.download()
+        
+        await m.edit_text("<blockquote><b>ᴛᴇꜱᴛɪɴɢ ᴜᴘʟᴏᴀᴅ ꜱᴘᴇᴇᴅ...</b></blockquote>")
+        st.upload()
+        
+        results = st.results.dict()
+        return results, None
+        
+    except ConfigRetrievalError:
+        return None, "<blockquote><b>ᴜɴᴀʙʟᴇ ᴛᴏ ᴄᴏɴɴᴇᴄᴛ ᴛᴏ ꜱᴇʀᴠᴇʀꜱ ᴛᴏ ᴛᴇꜱᴛ ʟᴀᴛᴇɴᴄʏ.</b></blockquote>"
+    except Exception as e:
+        return None, f"<blockquote><b>ᴇʀʀᴏʀ: {str(e)}</b></blockquote>"
+
+@app.on_message(filters.command(["speedtest", "spt"]) & SUDOERS)
+@language
+async def speedtest_command(client, message: Message, _):
+    m = await message.reply_text("<blockquote><b>ꜱᴛᴀʀᴛɪɴɢ ꜱᴘᴇᴇᴅᴛᴇꜱᴛ....</b></blockquote>")
+    
+    results, error = await run_speedtest(m)
+    
+    if error:
+        await m.edit_text(error)
+        return
+    
+    try:
+        string_speed = f"""
+<blockquote><b><u>ꜱᴘᴇᴇᴅᴛᴇꜱᴛ ɪɴꜰᴏ</u></b>
+<b>ᴜᴘʟᴏᴀᴅ: <code>{get_readable_file_size(results['upload'] / 8)}/s</code></b>
+<b>ᴅᴏᴡɴʟᴏᴀᴅ: <code>{get_readable_file_size(results['download'] / 8)}/s</code></b>
+<b>ᴘɪɴɢ: <code>{results['ping']} ᴍꜱ</code></b>
+<b>ᴅᴀᴛᴀ ꜱᴇɴᴛ: <code>{get_readable_file_size(int(results['bytes_sent']))}</code></b>
+<b>ᴅᴀᴛᴀ ʀᴇᴄᴇɪᴠᴇᴅ: <code>{get_readable_file_size(int(results['bytes_received']))}</code></b></blockquote>\n<blockquote><b><u>ꜱᴇʀᴠᴇʀ ɪɴꜰᴏ</u></b>
+<b>ɴᴀᴍᴇ: <code>{results['server']['name']}</code></b>
+<b>ᴄᴏᴜɴᴛʀʏ: <code>{results['server']['country']}, {results['server']['cc']}</code></b>
+<b>ꜱᴘᴏɴꜱᴏʀ: <code>{results['server']['sponsor']}</code></b>
+<b>ʟᴀᴛᴇɴᴄʏ: <code>{results['server']['latency']}</code></b>
+<b>ʟᴀᴛɪᴛᴜᴅᴇ: <code>{results['server']['lat']}</code></b>
+<b>ʟᴏɴɢɪᴛᴜᴅᴇ: <code>{results['server']['lon']}</code></b></blockquote>\n<blockquote><b><u>ᴄʟɪᴇɴᴛ ɪɴꜰᴏ</u></b>
+<b>ɪᴘ ᴀᴅᴅʀᴇꜱꜱ: <code>ʜɪᴅᴅᴇɴ</code></b>
+<b>ʟᴀᴛɪᴛᴜᴅᴇ: <code>{results['client']['lat']}</code></b>
+<b>ʟᴏɴɢɪᴛᴜᴅᴇ: <code>{results['client']['lon']}</code></b>
+<b>ᴄᴏᴜɴᴛʀʏ: <code>{results['client']['country']}</code></b>
+<b>ɪꜱᴘ: <code>{results['client']['isp']}</code></b>
+<b>ɪꜱᴘ ʀᴀᴛɪɴɢ: <code>{results['client']['isprating']}</code></b></blockquote>
+"""
+        await m.edit_text(string_speed)
+        
+    except Exception as e:
+        await m.edit_text(f"<blockquote><b>ᴇʀʀᴏʀ ᴘʀᴏᴄᴇꜱꜱɪɴɢ ʀᴇꜱᴜʟᴛꜱ: {str(e)}</b></blockquote>")
